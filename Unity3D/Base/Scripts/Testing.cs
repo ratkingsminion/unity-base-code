63067a84308c923be69cfaab46ce76c1e8aff08a
F.H <friedrich@ratking.de>
2016-05-21 17:53:47 +0200

    updated Testing.cs
    added possibility to create many screenshots successively

--- Unity3D/Base/Scripts/Testing.cs
+++ Unity3D/Base/Scripts/Testing.cs
@@ -5,10 +5,19 @@ namespace RatKing.Base {
 
 	public class Testing : MonoBehaviour {
 		public string screenshotPrefix = "screen";
-		
+		public float screenshotTakeRate = 0.25f;
+		bool takingScreenshots;
+
 		//
-		
+
 		void Update() {
+			if (screenshotTakeRate > 0f && Input.GetKeyDown(KeyCode.F11)) {
+				if (!takingScreenshots)
+					StartCoroutine("ScreenshotTakerCR");
+				else
+					StopCoroutine("ScreenshotTakerCR");
+				takingScreenshots = !takingScreenshots;
+			}
 			if (Input.GetKeyDown(KeyCode.F12)) {
 				Base.Helpers.Debug.CreateScreenshot(screenshotPrefix);
 			}
@@ -17,6 +26,13 @@ namespace RatKing.Base {
 			}
 		}
 
+		IEnumerator ScreenshotTakerCR() {
+			var wait = new WaitForSeconds(screenshotTakeRate);
+			while (true) {
+				Helpers.Debug.CreateScreenshot(screenshotPrefix, "screenshots/series");
+				yield return wait;
+			}
+		}
 	}
 	
 }
