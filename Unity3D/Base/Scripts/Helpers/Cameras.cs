ffb8394d1670a046e18b377fc5b30aa1548a1411
F.H <friedrich@ratking.de>
2016-08-29 20:35:26 +0200

    Small change to Cameras.cs
    Making GetWideFOV() an extension method

--- Unity3D/Base/Scripts/Helpers/Cameras.cs
+++ Unity3D/Base/Scripts/Helpers/Cameras.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 namespace RatKing.Base.Helpers {
 
 	public static class Cameras {
-		public static float GetWideFOV(Camera cam) {
+		public static float GetWideFOV(this Camera cam) {
 			return GetWideFOV(cam.fieldOfView, Screen.width * cam.rect.width, Screen.height * cam.rect.height);
 		}
 		public static float GetWideFOV(float fov, float w = -1f, float h = -1f) {
