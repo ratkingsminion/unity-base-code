2970fff108c3a8ab3f8cc4e1249bb47d29f78223
F.H <friedrich@ratking.de>
2016-10-20 10:58:39 +0200

    Sound helper function
    added sound helper function to convert to dB and back

---
+++ Unity3D/Base/Scripts/Helpers/Sound.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace RatKing.Base.Helpers {
+
+	public static class Sound {
+		// source: https://twitter.com/i/web/status/785798323038937088
+		public static float VolumeToDB(float volume) {
+			if (volume <= 0f) { return -80f; }
+			return 20f * Mathf.Log10(volume);
+		}
+		public static float DBToVolume(float dB) {
+			return Mathf.Pow(10f, db * 0.05f);
+		}
+	}
+
+}
