8b31d404df79063296e8ff1223bbd83b48772b31
F.H <friedrich@ratking.de>
2017-06-28 09:50:50 +0200

    added hook for nicer number formatting

---
+++ Unity3D/Base/Scripts/_thirdParty/Editor/UnityHook.cs
@@ -0,0 +1,12 @@
+﻿using UnityEditor;
+using System.Reflection;
+
+// source: https://twitter.com/PsyKola/status/874623012141768704
+
+[InitializeOnLoad]
+public class UnityHook {
+	static UnityHook() {
+		typeof(EditorGUI).GetField("kFloatFieldFormatString", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, "0.#######");
+		typeof(EditorGUI).GetField("kDoubleFieldFormatString", BindingFlags.Static | BindingFlags.NonPublic).SetValue(null, "0.##############");
+	}
+}
