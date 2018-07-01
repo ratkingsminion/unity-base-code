21c4182d2638edc15149356d4d52b54b066f238f
Friedrich Hanisch <friedrich@ratking.de>
2018-07-01 22:35:55 +0200

    better randomness
    DataStructures can now be shuffled and randomly indexed by your own generator if needed

--- Unity3D/Base/Scripts/Helpers/DataStructures.cs
+++ Unity3D/Base/Scripts/Helpers/DataStructures.cs
@@ -6,21 +6,29 @@ namespace RatKing.Base {
 	
 	public static class DataStructures {
 		static System.Random randomGenerator;
-		public static void Shuffle<T>(this IList<T> ls) {
-			if (randomGenerator == null)
-				randomGenerator = new System.Random();
+
+		public static void Shuffle<T>(this IList<T> ls, System.Random generator = null) {
+			if (generator == null) {
+				if (randomGenerator == null) { randomGenerator = new System.Random(); }
+				generator = randomGenerator;
+			}
 			int n = ls.Count;
 			while (n > 1) {
 				n--;
-				int k = randomGenerator.Next(n + 1);
+				int k = generator.Next(n + 1);
 				T value = ls[k];
 				ls[k] = ls[n];
 				ls[n] = value;
 			}
 		}
-		public static T GetRandomElement<T>(this HashSet<T> hs) {
-			if (hs.Count != 0) {
-				int r = Random.Range(0, hs.Count);
+
+		public static T GetRandomElement<T>(this HashSet<T> hs, System.Random generator = null)	 {
+			if (hs.Count > 0) {
+				if (generator == null) {
+					if (randomGenerator == null) { randomGenerator = new System.Random(); }
+					generator = randomGenerator;
+				}
+				int r = generator.Next(0, hs.Count);
 				foreach (T elem in hs)
 					if (r-- <= 0)
 						return elem;
@@ -28,20 +36,29 @@ namespace RatKing.Base {
 			return default(T);
 		}
 
-		public static T GetRandomElement<T>(this List<T> ls) {
+		public static T GetRandomElement<T>(this List<T> ls, System.Random generator = null) {
 			if (ls.Count == 0) { return default(T); }
-			return ls[Random.Range(0, ls.Count)];
+			if (generator == null) {
+				if (randomGenerator == null) { randomGenerator = new System.Random(); }
+				generator = randomGenerator;
+			}
+			return ls[generator.Next(0, ls.Count)];
 		}
 
-		public static KeyValuePair<U, T> GetRandomElement<U, T>(this Dictionary<U, T> ds) {
-			if (ds.Count != 0) {
-				int r = Random.Range(0, ds.Count);
+		public static KeyValuePair<U, T> GetRandomElement<U, T>(this Dictionary<U, T> ds, System.Random generator = null) {
+			if (ds.Count > 0) {
+				if (generator == null) {
+					if (randomGenerator == null) { randomGenerator = new System.Random(); }
+					generator = randomGenerator;
+				}
+				int r = generator.Next(0, ds.Count);
 				foreach (var elem in ds)
 					if (r-- <= 0)
 						return elem;
 			}
 			return default(KeyValuePair<U, T>);
 		}
+
 		public static T GetFirstElement<T>(this HashSet<T> hs) {
 			foreach (T elem in hs)
 				return elem;
