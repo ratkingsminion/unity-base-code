12ad199361dceae72d30204c1a151342150582fb
Friedrich Hanisch <friedrich@ratking.de>
2018-08-31 18:44:24 +0200

    fixed physics helper class
    renamed it so it doesn't clash with UnityEngine.Physics
    removed some Events code from other project

--- Unity3D/Base/Scripts/Helpers/PhysicsExtras.cs
+++ Unity3D/Base/Scripts/Helpers/PhysicsExtras.cs
@@ -4,8 +4,8 @@ using System.Collections.Generic;
 
 namespace RatKing.Base {
 
-	public class Physics {
-		public static void WakeUpEverythingAround(GameObject go, float padding, bool disableMyColliders) {
+	public class PhysicsExtras {
+		public static void WakeUpEverythingAround(GameObject go, float padding, bool disableMyColliders, int layerMaskPlayer) {
 			var colliders = go.GetComponentsInChildren<Collider>(false);
 			if (colliders.Length > 0) {
 				var bounds = colliders[0].bounds;
@@ -14,8 +14,7 @@ namespace RatKing.Base {
 					bounds.Encapsulate(c.bounds);
 				}
 				bounds.size += Vector3.one * padding * 2f;
-				foreach (var c in Physics.OverlapBox(bounds.center, bounds.extents, Quaternion.identity, layerMaskNotPlayer, QueryTriggerInteraction.Collide)) {
-					Events.Broadcast(c.gameObject, EventID.NOTIFY_WAKEUP, go);
+				foreach (var c in Physics.OverlapBox(bounds.center, bounds.extents, Quaternion.identity, ~layerMaskPlayer, QueryTriggerInteraction.Collide)) {
 					var r = c.attachedRigidbody;
 					if (r != null) { r.WakeUp(); }
 				}
@@ -25,10 +24,9 @@ namespace RatKing.Base {
 			}
 		}
 		
-		public static void WakeUpEverythingAround(Vector3 pos, Vector3 size, GameObject go) {
-			foreach (var c in Physics.OverlapBox(pos, size * 0.5f, Quaternion.identity, layerMaskNotPlayer, QueryTriggerInteraction.Collide)) {
+		public static void WakeUpEverythingAround(Vector3 pos, Vector3 size, GameObject go, int layerMaskPlayer) {
+			foreach (var c in Physics.OverlapBox(pos, size * 0.5f, Quaternion.identity, ~layerMaskPlayer, QueryTriggerInteraction.Collide)) {
 				//Base.QuickGizmos.DrawBox(pos, Quaternion.identity, size * 0.5f, Color.yellow, 1f);
-				Events.Broadcast(c.gameObject, EventID.NOTIFY_WAKEUP, go);
 				var r = c.attachedRigidbody;
 				if (r != null) { r.WakeUp(); }
 			}
