9e7640f2dda90eca521199c10e6261d9a4cbbb40
Friedrich Hanisch <friedrich@ratking.de>
2020-03-02 15:01:24 +0100

    add drawers (for editor) for Position2 and Position3

---
+++ Unity3D/Base/Editor/Position2Drawer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RatKing.Base {
+
+	[CustomPropertyDrawer(typeof(Position2))]
+	public class Position2PropertyDrawer : PropertyDrawer {
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+			var indent = EditorGUI.indentLevel;
+			EditorGUI.indentLevel = 0;
+			
+			var width = position.width * 0.5f - 2f;
+			var xRect = new Rect(position.x, position.y, width, position.height);
+			var yRect = new Rect(position.x + width + 4f, position.y, width, position.height);
+			
+			EditorGUIUtility.labelWidth = 12f;
+			EditorGUI.PropertyField(xRect, property.FindPropertyRelative("x"), new GUIContent("X"));
+			EditorGUI.PropertyField(yRect, property.FindPropertyRelative("y"), new GUIContent("Y"));
+
+			EditorGUI.indentLevel = indent;
+
+			EditorGUI.EndProperty();
+		}
+	}
+
+}
---
+++ Unity3D/Base/Editor/Position3Drawer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace RatKing.Base {
+
+	[CustomPropertyDrawer(typeof(Position3))]
+	public class Position3PropertyDrawer : PropertyDrawer {
+		public override void OnGUI(Rect position, SerializedProperty property, GUIContent label) {
+			EditorGUI.BeginProperty(position, label, property);
+			position = EditorGUI.PrefixLabel(position, GUIUtility.GetControlID(FocusType.Passive), label);
+			var indent = EditorGUI.indentLevel;
+			EditorGUI.indentLevel = 0;
+			
+			var width = position.width / 3f - 1f;
+			var xRect = new Rect(position.x, position.y, width, position.height);
+			var yRect = new Rect(position.x + width + 1.5f, position.y, width, position.height);
+			var zRect = new Rect(position.x + 2f * width + 3f, position.y, width, position.height);
+			
+			EditorGUIUtility.labelWidth = 10f;
+			EditorGUI.PropertyField(xRect, property.FindPropertyRelative("x"), new GUIContent("X"));
+			EditorGUI.PropertyField(yRect, property.FindPropertyRelative("y"), new GUIContent("Y"));
+			EditorGUI.PropertyField(zRect, property.FindPropertyRelative("z"), new GUIContent("Z"));
+
+			EditorGUI.indentLevel = indent;
+
+			EditorGUI.EndProperty();
+		}
+	}
+
+}
