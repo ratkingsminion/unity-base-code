d2209a7e75e846ae4b7bff1c9e0197ff74bec144
Friedrich Hanisch <friedrich@ratking.de>
2020-05-04 09:24:12 +0200

    update MaterialExtras, can now change shaders

--- Unity3D/Base/Scripts/Helpers/MaterialExtras.cs
+++ Unity3D/Base/Scripts/Helpers/MaterialExtras.cs
@@ -7,24 +7,42 @@ namespace RatKing.Base {
 	public static class MaterialExtras {
 
 		public class Pool {
-			private static Dictionary<Material, Dictionary<Color, Material>> usedMaterials = new Dictionary<Material, Dictionary<Color, Material>>();
-			public static void Add(Material material) {
-				if (!usedMaterials.ContainsKey(material))
-					usedMaterials[material] = new Dictionary<Color, Material>();
-				usedMaterials[material][material.color] = material;
-			}
-			public static Material Get(Material material, Color color) {
-				if (!usedMaterials.ContainsKey(material))
-					usedMaterials[material] = new Dictionary<Color, Material>();
+			static Dictionary<Material, Dictionary<Color, Material>> usedMaterialByColor = new Dictionary<Material, Dictionary<Color, Material>>();
+			static Dictionary<Material, Dictionary<Shader, Material>> usedMaterialByShader = new Dictionary<Material, Dictionary<Shader, Material>>();
+			static Dictionary<Material, Material> usedMaterialByShader_R = new Dictionary<Material, Material>();
 
-				if (!usedMaterials[material].ContainsKey(color)) {
+			public static Material Get(Material material, Color color) {
+				if (!usedMaterialByColor.ContainsKey(material))
+					usedMaterialByColor[material] = new Dictionary<Color, Material>();
+
+				if (!usedMaterialByColor[material].ContainsKey(color)) {
 					Material newMaterial = (Material)Material.Instantiate(material);
 					newMaterial.color = color;
-					usedMaterials[material][color] = newMaterial;
+					usedMaterialByColor[material][color] = newMaterial;
 					return newMaterial;
 				}
 
-				return usedMaterials[material][color];
+				return usedMaterialByColor[material][color];
+			}
+			
+			public static Material Get(Material material, Shader shader) {
+				if (!usedMaterialByShader.ContainsKey(material)) {
+					usedMaterialByShader[material] = new Dictionary<Shader, Material>();
+				}
+				if (!usedMaterialByShader[material].ContainsKey(shader)) {
+					Material newMaterial = (Material)Material.Instantiate(material);
+					newMaterial.shader = shader;
+					usedMaterialByShader[material][shader] = newMaterial;
+					usedMaterialByShader_R[newMaterial] = material;
+					return newMaterial;
+				}
+				return usedMaterialByShader[material][shader];
+			}
+			public static Material Reset(Material manipulatedMaterial) {
+				if (!usedMaterialByShader_R.ContainsKey(manipulatedMaterial)) {
+					return null;
+				}
+				return usedMaterialByShader_R[manipulatedMaterial];
 			}
 		}
 
